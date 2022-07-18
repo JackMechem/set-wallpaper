a17083a1c8f342cd7ca4abec036ce9263f20d2d8
JackMechem <mechemjack@gmail.com>
2022-07-18 13:02:52 -0700

    first commit

---
+++ set-wallpaper.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ "$1" == "help" ]
+then
+	printf "\nset-wallpaper.sh by Jack Mechem\nhttps://github.com/JackMechem\n\n\nset-wallpaper [ Directory/to/wallpaper.jpg | help | install ]\n\n"
+	printf "help - Help page\ninstall - Copies the script into /usr/local/bin (requires sudo)\n\n\n"
+	exit
+fi
+
+if [ "$1" == "install" ]
+then
+	sudo cp set-wallpaper.sh /usr/local/bin/set-wallpaper
+	exit
+fi
+
+wallpaper="$1"
+
+wal -i "$wallpaper" -n &
+
+monitorcount=`xrandr --query | grep -ow 'connected' | wc -l`
+
+for((i=0;i<=$(($monitorcount-1));i++)) do
+	nitrogen --set-zoom-fill --head="$i" "$wallpaper"
+done
+exit
